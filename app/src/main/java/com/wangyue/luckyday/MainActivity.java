c76ccaf75fa0608a64dde0152a146188981e4606
szwyvern163 <szwyvern@163.com>
2017-04-18 17:55:39 +0800

    1.popup a datepicker when click on the title of the calendar. You can select year quickly 2.limit date from 2016-01-01 to 2019-12-31 3.compatible to android 4.1 and 4" screen.  Make the view a little smaller

--- app/src/main/java/com/wangyue/luckyday/MainActivity.java
+++ app/src/main/java/com/wangyue/luckyday/MainActivity.java
@@ -1,9 +1,14 @@
 package com.wangyue.luckyday;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.LocaleList;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
@@ -18,12 +23,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import static java.lang.System.lineSeparator;
@@ -39,21 +46,41 @@ public class MainActivity extends AppCompatActivity implements OnDateSelectedLis
     TextView showLucky;
     TextView showUnLucky;
     TextView backToToday;
+    CalendarDay currentShowMonth = CalendarDay.from(new Date());
+    DatePickerDialog.OnDateSetListener dateSetListener ;
+    DatePickerDialog datePickerDialog;
+
+    View.OnClickListener calendarTitleClicked ;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            getSupportActionBar().setTitle(R.string.app_name);
+        }
         calendarView = (MaterialCalendarView) findViewById(R.id.calendarView);
         calendarView.setOnDateChangedListener(this);
         calendarView.setOnMonthChangedListener(this);
 
-        calendarView.setTileHeightDp(30);
+
+        calendarView.setTileHeightDp(28);
         calendarView.setDateSelected(new Date(),true);
-        calendarView.state().edit()
-                .setMinimumDate(CalendarDay.from(2009, 1, 1))
-                .setMaximumDate(CalendarDay.from(2019, 12, 31))
-                .commit();
+        SimpleDateFormat fmt=new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date minDate = fmt.parse(minDateStr);
+            Date maxDate = fmt.parse(maxDateStr);
+            calendarView.state().edit()
+            .setMinimumDate(minDate)  //只准备了2016年到2019年的数据
+                    .setMaximumDate(maxDate)
+                    .commit();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
         //Setup initial text
         showDateText = (TextView) findViewById(R.id.showDateText);
         //showDateText.setText(getSelectedDatesString());
@@ -72,43 +99,84 @@ public class MainActivity extends AppCompatActivity implements OnDateSelectedLis
 
             public void onClick(View arg0) {
                 Date now = new Date();
-                calendarView.clearSelection();
-                calendarView.setDateSelected(now,true);
-                calendarView.setCurrentDate(now);
-
+                setCalendarViewDate(now);
                 refreshDesc(now);
 
 
             }
 
         });
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                System.out.println(year +"  "+month+"  "+dayOfMonth);
+                CalendarDay day = CalendarDay.from(year,month,dayOfMonth);
+                setCalendarViewDate(day.getDate());
+                refreshDesc(day.getDate());
+            }
+        };
+
+
+        calendarTitleClicked = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Date selectedDate = calendarView.getSelectedDate().getDate();
+                //DatePicker datePicker = new DatePicker();
+                datePickerDialog = new DatePickerDialog(
+                        //MainActivity.this, android.R.style.Theme_Material_Light_Dialog_Alert, dateSetListener , currentShowMonth.getYear(), currentShowMonth.getMonth(), currentShowMonth.getDay()
+                        MainActivity.this, 0, dateSetListener , currentShowMonth.getYear(), currentShowMonth.getMonth(), currentShowMonth.getDay()
+
+                );
+                SimpleDateFormat fmt=new SimpleDateFormat("yyyy-MM-dd");
+                try {
+                    Date minDate = fmt.parse(minDateStr);
+                    Date maxDate = fmt.parse(maxDateStr);
+                    datePickerDialog.getDatePicker().setMinDate(minDate.getTime());
+                    datePickerDialog.getDatePicker().setMaxDate(maxDate.getTime());
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                //System.out.println(datePickerDialog);
+                datePickerDialog.show();
+                //   System.out.println(currentShowMonth);
+
+            }
+        };
+        calendarView.setOnTitleClickListener(calendarTitleClicked);
+
+
+
         refreshDesc(new Date());
     }
 
 
     @Override
     public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-       // showDateText.setText(getSelectedDatesString());
+
         refreshDesc(date.getDate());
-        //Map<String, List<String>> map = getByDate(2017,3,23,"zh_CN");
 
 
     }
 
     @Override
     public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
-
-        getSupportActionBar().setTitle("LuckyDay");
+        currentShowMonth = date;
+        //getSupportActionBar().setTitle("LuckyDay");
     }
 
 
+
+
+
     private static final DateFormat FORMATTER = SimpleDateFormat.getDateInstance(SimpleDateFormat.LONG);
-    private String getSelectedDatesString() {
-        CalendarDay date = calendarView.getSelectedDate();
-        if (date == null) {
-            return "No Selection";
-        }
-        return FORMATTER.format(date.getDate());
+
+
+    public void setCalendarViewDate(Date date){
+        calendarView.clearSelection();
+        calendarView.setDateSelected(date,true);
+        calendarView.setCurrentDate(date);
     }
 
 
@@ -119,6 +187,26 @@ public class MainActivity extends AppCompatActivity implements OnDateSelectedLis
 
         Calendar c = Calendar.getInstance();
         c.setTime(date);
+
+        System.out.println("locale kkk");
+        Locale localea = Locale.GERMAN;
+        System.out.println(localea);
+       // System.out.println(locale.getCountry().toString());
+        //locale = getResources().getConfiguration().getLocales().get(0);
+        System.out.println("run api is "+Build.VERSION.SDK_INT);
+        System.out.println("build api is "+Build.VERSION_CODES.N);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+
+            LocaleList al = getResources().getConfiguration().getLocales();
+            System.out.println(al.toString());
+            localea = getResources().getConfiguration().getLocales().get(0);
+            System.out.println(localea);
+        }else
+        {
+            //locale = MainActivity.this.getResources().getConfiguration().locale;
+        }
+        System.out.println(localea.getCountry());
+        System.out.println("locale rrrkkk");
 
         Map<String, List<String>> map = getByDate(c.get(Calendar.YEAR),c.get(Calendar.MONTH)+1, c.get(Calendar.DAY_OF_MONTH),"zh_CN");
         String luckyStr = map.get(DO).toString();
@@ -132,7 +220,7 @@ public class MainActivity extends AppCompatActivity implements OnDateSelectedLis
 
 
         SimpleDateFormat fmt=new SimpleDateFormat("yyyy-MM-dd");
-        if(fmt.format(date).toString().equals(fmt.format(new Date()).toString())){
+        if(fmt.format(date).equals(fmt.format(new Date()))){
             backToToday.setVisibility(View.INVISIBLE);
         }else {
             backToToday.setVisibility(View.VISIBLE);
@@ -141,8 +229,14 @@ public class MainActivity extends AppCompatActivity implements OnDateSelectedLis
 
     }
 
+
+    //下面是读数据的代码段
     final static String DO = "DO";
     final static String NOT_DO = "NOT_DO";
+
+    final static String minDateStr = "2016-01-01";
+    final static String maxDateStr = "2019-12-31";
+    //final static Date minDate = new SimpleDateFormat("yyyy-MM-dd").parse("2016-01-01");
 
     /**
      * Core algorithm method
